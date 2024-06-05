f9ee4451494663744ff2edb1dc48fdff00f92c44
Willyamdevon <dezhintern@gmail.com>
2024-06-05 10:03:43 +0300

    error:(

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"API/internal/config"
+	"API/internal/lib/logger/sl"
+	"API/internal/storage/sqlite"
 	"log/slog"
 	"os"
 )
@@ -22,9 +24,13 @@ func main() {
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
 
-	// TODO: init logger: slog
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
 
-	// TODO: init storage: sqlite
+	_ = storage
 
 	// TODO: init router: chi, "chi render"
 
